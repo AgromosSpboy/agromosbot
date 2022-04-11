fbd12ebb474783ee6fd2d151c498e3c16d9403e9
AgromosSpboy <103408838+AgromosSpboy@users.noreply.github.com>
2022-04-11 05:24:11 -0400

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -1,15 +1,11 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
-*Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de AGROMOS SP - Bot*
+*Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de  Sapit Bot*
 
 *➤ Grupo oficial del Bot:*
 *1.-https://chat.whatsapp.com/CBoAGmEFcCy44xIoWBSgzJ
 
-*➤ Grupo2 oficial del Bot:*
-*2.-https://chat.whatsapp.com/FZsoX7iGBZLBQgFX9MUXIy
-*➤ Grupo3 oficial del Bot:*
-*3.-https://chat.whatsapp.com/K0UCo7igED92q9c1w2DiEH
 `.trim() 
-conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*🔥 AGROMOS SP - BOT 🔥*', 'status@broadcast')}
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*🔥 Sapito BoT 🔥*', 'status@broadcast')}
 handler.command = /^(grupos|gruposofc|gruposofc)$/i
 module.exports = handler
